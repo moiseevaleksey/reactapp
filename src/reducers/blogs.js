9891d798aff06e48e66bd5b0bde4744ac3ab937b
Aliaksei_Maiseyeu <moiseevaleskey92@gmail.com>
2018-03-09 18:26:46 +0300

    load blogs

--- src/reducers/blogs.js
+++ src/reducers/blogs.js
@@ -1,32 +1,4 @@
-const initState = [
-  {
-    id: 1,
-    title: 'Title1',
-    text: 'Text1',
-  },
-  {
-    id: 2,
-    title: 'Title2',
-    text: 'Text2',
-  },
-  {
-    id: 3,
-    title: 'Title3',
-    text: 'Text3',
-  },
-  {
-    id: 4,
-    title: 'Title4',
-    text: 'Text4',
-  },
-  {
-    id: 5,
-    title: 'Title5',
-    text: 'Text5',
-  },
-];
-
-export default function blogList(state = initState, action) {
+export default function carList(state = [], action) {
   if (action.type === 'ADD_BLOG') {
     const maxId = state.reduce((a, b) => {
       return a.id > b.id ? a.id : b.id;
@@ -37,6 +9,9 @@ export default function blogList(state = initState, action) {
     ];
   } else if (action.type === 'DELETE_BLOG') {
     const newState = state.filter(blog => blog.id !== action.id);
+    return newState;
+  } else if (action.type === 'LOAD_BLOGS') {
+    const newState = action.data;
     return newState;
   }
   return state;
